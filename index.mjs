3f11ba741ee169f636385c4837969ac010d1c882
sabaFitwi <fitwisaba@gmail.com>
2022-11-16 14:54:03 +0100

    carsousel functionality added

---
+++ index.mjs
@@ -0,0 +1,8 @@
+/**
+ * --------------------------------------------------------------------------
+ * Bootstrap (v5.2.2): carousel.js
+  * --------------------------------------------------------------------------
+ */
+import bootstrap from "/node_modules/bootstrap/js/src/carousel.js"
+var myCarousel = document.querySelector("#header")
+var carousel = new bootstrap.Carousel(myCarousel)
