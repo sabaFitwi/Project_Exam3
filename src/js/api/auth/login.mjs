b0ae0f03c08c72ecb76726aa5f0b0c29cfa774c7
sabaFitwi <fitwisaba@gmail.com>
2022-11-29 16:55:15 +0100

    redirect to profile

--- src/js/api/auth/login.mjs
+++ src/js/api/auth/login.mjs
@@ -1,6 +1,6 @@
 import { Auction_API_URL } from "../constant.mjs";
 import { headers } from "../headers.mjs";
-import { save } from "../../storage/localStorage.mjs";
+import { load, save } from "../../storage/localStorage.mjs";
 
 
 
@@ -20,6 +20,8 @@ form.addEventListener("submit", (event) => {
 
 
 
+
+
     async function login(profile) {
         const options = {
             method: "post",
@@ -35,10 +37,21 @@ form.addEventListener("submit", (event) => {
             save("token", profile.accessToken);
             delete profile.accessToken;
             save("profile", profile);
+            load("profile", profile.name)
+            window.location.href = (`/profile/?view=profile&name=${profile.name}`);
+
+            // if (token === "undefined" || token === null) {
+            //     console.log("error email or password");
+            //     /**
+            //      * Displays a message to the user
+            //      */
+            //     errorMessage.innerHTML = ` <div> <p btn-danger>Invalid email or password</p></div>`;
+            // }
             return profile;
         }
 
         throw new Error(response.statusText);
+
     }
 
     login(user);
