a08201af8d45fbca45d7a17618511f8052a84647
sabaFitwi <fitwisaba@gmail.com>
2022-12-04 19:13:50 +0100

    commit update change

--- src/js/listing/getListings.mjs
+++ src/js/listing/getListings.mjs
@@ -9,7 +9,7 @@ export async function getListings(limit = 20, offset = 0) {
     headers: headers("application/json"),
   };
   const response = await fetch(
-    `${Auction_API_URL}/listings?limit=${limit}&offset=${offset}`,
+    `${Auction_API_URL}/listings?limit=${limit}&offset=${offset}&sort=created&sortOrder=desc`,
     options
   );
   const data = await response.json();
@@ -29,22 +29,32 @@ function getListingsTemplet(listings) {
   listingsDiv.innerHTML = "";
   if (listings) {
     listings.map((listing) =>
-      listingsDiv.innerHTML += `<a href="view-detail/?id=${listing.id}" class="col-md-4 col-lg-4 col-xl-3 p-2 shadow new">
-            <div class="container border-0 ">
-              <img src="${listing.media}" class="img-thumbnail" />
-               
-              
-  
-              <div class="text-center">
-                <h4 class="text-capitalize large-font my-1">${listing.title}</h4>
-                <p >${listing.description}</p>
-                <p fw-bold>End date: <span text-primary f-large>${listing.endsAt}</span></p>
-               <button type="buttom" class="btn btn-primary my-3"> view Detail
-                  <span> Bid(${listing._count.bids})</span> 
-                </button>
-              </div>
-            </div>
-         </a>`
+      listingsDiv.innerHTML += `<a href="view-detail/?id=${listing.id}" class="col-md-4 col-lg-4 col-xl-3 p-2 mt-5 shadow new">
+          <div class="container border-0 ">
+             <img  id="img" src="${listing.media[0]}"  class="img-thumbnail listing-image"  />
+            <div class="text-center">
+             <h4 class="text-capitalize  my-1">${listing.title}</h4>
+             <p >${listing.description}</p>
+             <p fw-bold>End date: <span text-primary f-large>${listing.endsAt}</span></p>
+             <button type="buttom" class="btn btn-primary my-3"> view Detail
+             <span> Bid(${listing._count.bids})</span>
+              </button>
+           </div>
+          </div >
+         </a > `
     )
   }
-} 
+}
+
+// const images = document.querySelectorAll('img');
+
+// images.forEach(img => {
+
+//   img.addEventListener('error', function handleError() {
+//     const defaultImage =
+//       "https://picsum.photos/id/1/200/300";
+//     console.log(defaultImage)
+//     img.src = defaultImage;
+//     img.alt = 'default';
+//   });
+// });
